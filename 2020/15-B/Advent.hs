db0c5708177aea2f0160b6c3e8c5045cb7337bf7
Joris Vandermeersch <joris@vdmr.be>
2020-12-15 11:07:35 +0100

    Add 2020 15

---
+++ 2020/15-B/Advent.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Data.List as L
+import Data.List.Split
+import Data.Map.Strict as M
+
+-- calculate 2020 mem cur [] = cur
+calculate 30000000 mem cur [] = cur
+calculate n mem cur [] | member cur mem = calculate (n+1) (M.insert cur n mem) (n - (mem ! cur)) []
+                       | otherwise = calculate (n+1) (M.insert cur n mem) 0 []
+calculate n mem cur (x:xs) = calculate (n+1) (M.insert cur n mem) x xs
+
+setup (x:xs) = calculate 1 M.empty x xs
+
+main = do
+  cnt <- getContents
+  print $ L.map (setup . L.map read . splitOn ",") $ lines cnt
+
