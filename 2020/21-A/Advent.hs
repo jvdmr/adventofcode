1314e80fb519f33a3b63a4047fe96030cd7dbe6a
Joris Vandermeersch <joris@vdmr.be>
2020-12-21 17:00:05 +0100

    Add 2020 21

---
+++ 2020/21-A/Advent.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Data.List hiding (insert, empty)
+import Data.List.Split
+import Data.Map.Strict ((!), insert, empty, member)
+import qualified Data.Map.Strict as M
+
+flatten :: [[a]] -> [a]
+flatten = foldl (++) []
+
+data Food = Food {ingredients::[String], allergens::[String]}
+  deriving (Show, Eq, Ord)
+
+parseFood line = Food (words a) (splitOn ", " $ init b)
+  where (a:b:[]) = splitOn " (contains " line
+
+foodAllergens (Food i a) = map ((,) i) a
+
+reduce mem [] = mem
+reduce mem ((is, a):foods) | member a mem = reduce (insert a (filter (flip elem is) $ mem ! a) mem) foods
+                           | otherwise = reduce (insert a is mem) foods
+
+
+findSafeIngredients foods = filter (not . flip elem suspectIngredients) allIngredients
+  where suspectIngredients = nub $ sort $ M.foldl (++) [] $ reduce empty $ flatten $ map foodAllergens foods
+        allIngredients = foldl (++) [] $ map ingredients foods
+
+main = do
+  cnt <- getContents
+  print $ length $ findSafeIngredients $ map parseFood $ lines cnt
+
