549e6dbb8a03f384b211f4af4005802775196999
Joris Vandermeersch <joris@vdmr.be>
2021-04-07 13:05:37 +0200

    Add 2015 02

---
+++ 2015/02-B/Advent.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+calcRibbon dimensions = l + l + w + w + l * w * h
+  where (l:w:h:[]) = sort $ map read $ splitOn "x" dimensions
+
+main = do
+  cnt <- getContents
+  print $ foldl (+) 0 $ map calcRibbon $ lines cnt
+
