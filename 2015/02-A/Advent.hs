9767d8b81a49a1ab03f5bcdcd08a313b0c2d7ce8
Joris Vandermeersch <joris@vdmr.be>
2021-04-07 13:05:37 +0200

    Add 2015 02

---
+++ 2015/02-A/Advent.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+calcArea dimensions = smallest + 2 * (front + side + top)
+  where (l:w:h:[]) = map read $ splitOn "x" dimensions
+        front = w * h
+        side = l * h
+        top = w * l
+        smallest = head $ sort [front, side, top]
+
+main = do
+  cnt <- getContents
+  print $ foldl (+) 0 $ map calcArea $ lines cnt
+
