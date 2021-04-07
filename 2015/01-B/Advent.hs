0e3393e0f5caf888c955c579ac0631010864bc95
Joris Vandermeersch <joris@vdmr.be>
2021-04-07 11:46:01 +0200

    Add 2015 01

---
+++ 2015/01-B/Advent.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Data.List
+
+findBasement p (-1) _ = p
+findBasement _ _ [] = 0
+findBasement p f ('(':rst) = findBasement (p + 1) (f + 1) rst
+findBasement p f (')':rst) = findBasement (p + 1) (f - 1) rst
+
+main = do
+  cnt <- getContents
+  print $ map (findBasement 0 0) $ lines cnt
+
