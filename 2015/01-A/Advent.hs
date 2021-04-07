8cbc8d15ecf1fe37957faee8010cbd9318a8cd4d
Joris Vandermeersch <joris@vdmr.be>
2021-04-07 11:46:01 +0200

    Add 2015 01

---
+++ 2015/01-A/Advent.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Data.List
+
+findFloor f [] = f
+findFloor f ('(':rst) = findFloor (f + 1) rst
+findFloor f (')':rst) = findFloor (f - 1) rst
+
+main = do
+  cnt <- getContents
+  print $ map (findFloor 0) $ lines cnt
+
