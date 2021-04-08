dbe611b76ba8f61e9b58765f7b94bd106991a7e8
Joris Vandermeersch <joris@vdmr.be>
2021-04-08 19:39:31 +0200

    Add 2015 04

---
+++ 2015/04-A/Advent.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Data.List
+import qualified Data.ByteString.Lazy.UTF8 as BLU
+import qualified Data.ByteString.Lazy as BL
+import Data.Digest.Pure.MD5
+
+correctHash key = "00000" == take 5 hashedKey
+  where hashedKey = show $ md5 $ BLU.fromString key
+
+mine n key | correctHash (key ++ show n) = n
+           | otherwise = mine (n + 1) key
+
+main = do
+  cnt <- getContents
+  print $ map (mine 1) $ lines cnt
+
