32648208b49f7ece1bd0df6474d23d0b32d1c53e
Joris Vandermeersch <joris@vdmr.be>
2021-04-16 10:43:15 +0200

    Add 2015 09

---
+++ 2015/09-B/Advent.hs
@@ -0,0 +1,71 @@
+module Main where
+
+import Data.List hiding (insert)
+import Data.Map (insert, (!), keys)
+import qualified Data.Map as M
+import Text.Parsec
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+flatten :: [[a]] -> [a]
+flatten [] = []
+flatten (a:rst) = a ++ flatten rst
+
+type Graph = M.Map City [Distance]
+
+data City = City String
+            deriving (Eq, Ord)
+
+instance Show City where
+  show (City a) = a
+
+data Distance = Distance (City, City) Int
+                deriving (Eq)
+
+instance Show Distance where
+  show (Distance (a, b) dist) = show a ++ " to " ++ show b ++ " = " ++ show dist
+
+instance Ord Distance where
+  compare (Distance _ distA) (Distance _ distB) = compare distA distB
+
+city =     many1 letter >>= return . City
+       <?> "city"
+
+distance =     city >>= \a -> string " to " >> city >>= \b -> string " = " >> many1 digit >>= return . Distance (a, b) . read
+           <?> "distance"
+
+parseString :: String -> Distance
+parseString = right . parse distance "(source)"
+  where right (Right ast) = ast
+        right (Left a) = error $ show a
+
+getDistance :: Distance -> Int
+getDistance (Distance _ d) = d
+
+getDestination :: Distance -> City
+getDestination (Distance (_, b) _) = b
+
+reverseDistance :: Distance -> Distance
+reverseDistance (Distance (a, b) d) = Distance (b, a) d
+
+(?!) :: Graph -> City -> [Distance]
+(?!) g k = M.findWithDefault [] k g
+
+createGraph :: Graph -> [Distance] -> Graph
+createGraph g [] = g
+createGraph g (dist@(Distance (a, b) d):rst) = createGraph (insert a (dist:(g ?! a)) (insert b ((reverseDistance dist):(g ?! b)) g)) rst
+
+doTsp :: Graph -> [City] -> City -> [[Distance]]
+doTsp g visited n | nextNodes == [] = [[]]
+                  | otherwise = flatten $ map (\next -> map (next:) (doTsp g (n:visited) $ getDestination next)) nextNodes
+                  where nextNodes = filter (not . flip elem visited . getDestination) $ (g ! n)
+
+tsp :: Graph -> [[Distance]]
+tsp g = flatten $ map (doTsp g []) ks
+  where ks = keys g
+
+main = do
+  cnt <- getContents
+  print $ last $ sort $ map (foldl (+) 0 . map getDistance) $ tsp $ createGraph M.empty $ idtrace $ map parseString $ lines cnt
+
