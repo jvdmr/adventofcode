cb819f347835b526964cd008fd1f5d90a8cd2009
Joris Vandermeersch <joris@vdmr.be>
2021-12-27 23:24:30 +0100

    Add 2015 20

---
+++ 2015/20-A/Advent.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Data.List
+
+import Debug.Trace
+idtrace x = trace (show x) x
+ftrace f x = trace (f x) x
+
+isqrt :: Int -> Int
+isqrt = floor . sqrt . fromIntegral
+
+factors :: Int -> [Int]
+factors n = nub . concat $ [[x, q] | x <- [1..isqrt n], let (q, r) = divMod n x, r == 0]
+
+presents' = (10 *) . sum . factors
+
+presents n = findIndex (>= n) $ map presents' [0..]
+
+main = do
+  cnt <- getContents
+  print $ map (presents . read) $ lines cnt
+
---
+++ 2015/20-B/Advent.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Data.List
+
+import Debug.Trace
+idtrace x = trace (show x) x
+ftrace f x = trace (f x) x
+
+isqrt :: Int -> Int
+isqrt = floor . sqrt . fromIntegral
+
+factors :: Int -> [Int]
+factors n = nub . concat $ [[x, q] | x <- [1..isqrt n], let (q, r) = divMod n x, r == 0]
+
+presents' house = (11 *) $ sum $ filter (> (house - 1) `div` 50) $ factors house
+
+presents n = findIndex (>= n) $ map presents' [0..]
+
+main = do
+  cnt <- getContents
+  print $ map (presents . read) $ lines cnt
+
