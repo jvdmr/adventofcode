8220686ec751baf7a1f593765eeadc72f726ad5c
Joris Vandermeersch <joris@vdmr.be>
2022-12-01 07:13:53 +0100

    Fix/add more dependencies

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cabal install --lib split
-cabal install --lib parsec
 cabal install --lib mtl
 cabal install --lib parallel
+cabal install --lib utf8-string
+cabal install --lib pureMD5
+cabal install --lib json
+cabal install --lib hashmap
+cabal install --lib parsec3-numbers
+# cabal install --lib strings
