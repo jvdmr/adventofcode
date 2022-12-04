6d5ea2557ddd86aca6cbf40413f8bec264556da8
Joris Vandermeersch <joris@vdmr.be>
2022-12-04 06:56:35 +0100

    Add 2022 04

---
+++ 2022/04-B/Advent.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+tuplify [x, y] = (x, y)
+
+expandRange :: String -> (Int, Int)
+expandRange = tuplify . map read . splitOn "-"
+
+parseTasks :: String -> [(Int, Int)]
+parseTasks = map expandRange . splitOn ","
+
+findWhollyContaining :: (Ord a) => [(a, a)] -> Bool
+findWhollyContaining [(a, b), (c, d)] | a <= c && b >= c = True
+                                      | a <= d && b >= d = True
+                                      | a >= c && b <= d = True
+                                      | otherwise = False
+
+main = do
+  cnt <- getContents
+  print $ length $ filter findWhollyContaining $ map parseTasks $ lines cnt
+
