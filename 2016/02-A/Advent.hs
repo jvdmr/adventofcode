ff46a49ba31786b78d5c6ce480a5912f0fe2a239
Joris Vandermeersch <joris@vdmr.be>
2022-12-06 00:56:17 +0100

    Add 2016 02

---
+++ 2016/02-A/Advent.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Data.List
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+keypad = ["123", "456", "789"]
+
+type Pos = (Int, Int)
+
+key (x, y) = keypad !! x !! y
+
+go :: Char -> Pos -> Pos
+go 'U' (x, y) = (max 0 (x - 1), y)
+go 'D' (x, y) = (min 2 (x + 1), y)
+go 'L' (x, y) = (x, max 0 (y - 1))
+go 'R' (x, y) = (x, min 2 (y + 1))
+
+nextKey :: (Pos, String) -> [Char] -> (Pos, String)
+nextKey (pos, out) [] = (pos, out ++ [key pos])
+nextKey (pos, out) (d:ds) = nextKey (go d pos, out) ds
+
+main = do
+  cnt <- getContents
+  print $ foldl nextKey ((1, 1), "") $ lines cnt
+
---
+++ 2016/02-B/Advent.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Data.List
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+keypad = ["       ", "   1   ", "  234  ", " 56789 ", "  ABC  ", "   D   ", "       "]
+
+type Pos = (Int, Int)
+
+key (x, y) = keypad !! x !! y
+
+go :: Char -> Pos -> Pos
+go 'U' (x, y) = (x - 1, y)
+go 'D' (x, y) = (x + 1, y)
+go 'L' (x, y) = (x, y - 1)
+go 'R' (x, y) = (x, y + 1)
+
+nextKey :: (Pos, String) -> [Char] -> (Pos, String)
+nextKey (pos, out) [] = (pos, out ++ [key pos])
+nextKey (pos, out) (d:ds) = nextKey (realnpos nk, out) ds
+  where npos = go d pos
+        nk = key npos
+        realnpos ' ' = pos
+        realnpos _ = npos
+
+main = do
+  cnt <- getContents
+  print $ foldl nextKey ((3, 1), "") $ lines cnt
+
