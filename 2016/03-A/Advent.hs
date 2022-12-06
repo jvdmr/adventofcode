7748ed3b169cf9c2f1e0c39ec0264242cd670ea7
Joris Vandermeersch <joris@vdmr.be>
2022-12-06 01:06:14 +0100

    Add 2016 03

---
+++ 2016/03-A/Advent.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Data.List
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+possible :: [Int] -> Bool
+possible [a, b, c] = a + b > c && a + c > b && b + c > a
+
+main = do
+  cnt <- getContents
+  print $ length $ filter possible $ map (map read . words) $ lines cnt
+
---
+++ 2016/03-B/Advent.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+possible :: [Int] -> Bool
+possible [a, b, c] = a + b > c && a + c > b && b + c > a
+
+main = do
+  cnt <- getContents
+  print $ length $ filter possible $ chunksOf 3 $ foldl (++) [] $ transpose $ map (map read . words) $ lines cnt
+
