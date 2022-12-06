e729a72e8c0ff6526d957750ae721f89bd71dcb8
Joris Vandermeersch <joris@vdmr.be>
2022-12-06 15:33:10 +0100

    Add 2016 07

---
+++ 2016/07-A/Advent.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+abba (a:r@(b:c:d:_)) | a /= b && a == d && b == c = True
+                     | otherwise = abba r
+abba _ = False
+
+abbas [] = False
+abbas [a] = abba a
+abbas (a:b:r) = not (abba b) && (abba a || abbas r)
+
+tls = abbas . splitOneOf "[]"
+
+main = do
+  cnt <- getContents
+  print $ length $ filter tls $ lines cnt
+
---
+++ 2016/07-B/Advent.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+evenuneven ls = [map snd $ filter (odd . fst) indexed, map snd $ filter (even . fst) indexed]
+  where indexed = zip [1..] ls
+
+bab (a:b:_) = [b, a, b]
+
+abas (a:r@(b:c:_)) | a /= b && a == c = [a, b, c]:abas r
+                   | otherwise = abas r
+abas _ = []
+
+ababab [abals, babls] = [] /= (filter (\s -> [] /= (filter (isInfixOf s) babls)) $ map bab $ concat $ map abas abals)
+
+ssl = ababab . evenuneven . splitOneOf "[]"
+
+main = do
+  cnt <- getContents
+  print $ length $ filter ssl $ lines cnt
+
