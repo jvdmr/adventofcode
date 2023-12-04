fd8d199a5681073122ee3b5b859a0d00b50ea119
Joris Vandermeersch <joris@vdmr.be>
2023-12-04 15:46:32 +0100

    Add 2023 04

---
+++ 2023/04-A/Advent.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Data.List
+import Data.List.Split (splitOn)
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+type Card = ([Int], [Int])
+
+parseCard :: String -> Card
+parseCard s = (head nrs, last nrs)
+  where nrs = map (map read . filter ((<) 0 . length) . splitOn " ") $ splitOn " | " $ last $ splitOn ": " s
+
+score :: Card -> Int
+score (winning, ours) | l > 0 = 2 ^ (l - 1)
+                      | otherwise = 0
+  where l = length $ filter (flip elem winning) ours
+
+main = do
+  cnt <- getContents
+  print $ sum $ map (idtrace . score . idtrace . parseCard) $ lines cnt
+
---
+++ 2023/04-B/Advent.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import Data.List
+import Data.List.Split (splitOn)
+import Data.Map ((!))
+import qualified Data.Map as M
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+type Card = ([Int], [Int])
+type Scores = M.Map Int Int
+
+parseCard :: String -> (Int, Card)
+parseCard s = (read n, (head nrs, last nrs))
+  where nrs = map (map read . filter ((<) 0 . length) . splitOn " ") $ splitOn " | " content
+        ['C':'a':'r':'d':' ':n, content] = splitOn ": " s
+
+calculate :: Scores -> (Int, Card) -> Scores
+calculate scores (n, (winning, ours)) = M.insert n (1 + score) scores
+  where matching = length $ filter (flip elem winning) ours
+        extraCards = filter (flip elem $ M.keys scores) $ take matching $ tail [n..]
+        score = sum $ map ((!) scores) extraCards
+
+parseCards :: [String] -> Scores
+parseCards = foldl calculate M.empty . reverse . map parseCard
+
+totalScore :: Scores -> Int
+totalScore s = sum $ M.elems s
+
+main = do
+  cnt <- getContents
+  print $ totalScore $ parseCards $ lines cnt
