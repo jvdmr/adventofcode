6648d52b75f8d87fd1a79d1dc7c8bac0981f6ad0
Joris Vandermeersch <joris@vdmr.be>
2023-12-10 14:27:49 +0100

    Add 2023 09

---
+++ 2023/09-A/Advent.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Data.List
+import Data.List.Split (splitOn)
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+diffs :: [Int] -> [Int]
+diffs [] = []
+diffs [a] = []
+diffs (a:b:rest) = (b - a):diffs (b:rest)
+
+nextInSeries :: [Int] -> Int
+nextInSeries ns | all (== 0) ns = 0
+                | otherwise = last ns + nextInSeries (diffs ns)
+
+main = do
+  cnt <- getContents
+  print $ sum $ map (nextInSeries . map read . splitOn " ") $ lines cnt
+
---
+++ 2023/09-B/Advent.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Data.List
+import Data.List.Split (splitOn)
+
+import Debug.Trace
+idtrace x = trace (show x) x
+
+diffs :: [Int] -> [Int]
+diffs [] = []
+diffs [a] = []
+diffs (a:b:rest) = (b - a):diffs (b:rest)
+
+prevInSeries :: [Int] -> Int
+prevInSeries ns | all (== 0) ns = 0
+                | otherwise = head ns - prevInSeries (diffs ns)
+
+main = do
+  cnt <- getContents
+  print $ sum $ map (prevInSeries . map read . splitOn " ") $ lines cnt
+
