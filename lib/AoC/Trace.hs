41a7451e490fe627daee927c4d4a9f26c64b0641
Joris Vandermeersch <joris@vdmr.be>
2024-11-25 11:42:03 +0100

    add rtrace to AoC.Trace lib

--- lib/AoC/Trace.hs
+++ lib/AoC/Trace.hs
@@ -2,6 +2,7 @@
 module AoC.Trace
   ( idtrace
   , ftrace
+  , rtrace
   ) where
 
 import Debug.Trace (trace)
@@ -11,4 +12,7 @@ idtrace x = trace (show x) x
 
 ftrace :: (a -> String) -> a -> a
 ftrace f x = trace (f x) x
+
+rtrace :: (Show a, Show b) => (a -> b) -> a -> b
+rtrace f n = ftrace (\r -> show n ++ " => " ++ show r) $ f n
 
