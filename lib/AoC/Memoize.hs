19957164359f21f65408e323ee4bceddb8187264
Joris Vandermeersch <joris@vdmr.be>
2024-11-25 11:43:40 +0100

    fix memoize

--- lib/AoC/Memoize.hs
+++ lib/AoC/Memoize.hs
@@ -11,7 +11,7 @@ import Vdmr.Trace
 type MemoizableFunction a b = ((a -> b) -> a -> b)
 
 memoize :: (Ord a) => MemoizableFunction a b -> [a] -> (a -> b)
-memoize f l = f!
+memoize f l = f'
   where mem = fromList [(n, f f' n) | n <- l]
         f' a = mem ! a
 
