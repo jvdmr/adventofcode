24452e48f18ac98db08a69605ad0a912cd561297
Joris Vandermeersch <joris@vdmr.be>
2024-11-29 18:42:58 +0100

    Committing another bad version of 23/21

--- src/AoC2023/Day21.hs
+++ src/AoC2023/Day21.hs
@@ -1,15 +1,15 @@
-{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, FlexibleContexts, TypeFamilies #-}
 module AoC2023.Day21
   ( part1
   , part2
   ) where
 
-import Data.List (intercalate)
+import Data.List (transpose)
 import qualified Data.Map as M
-import qualified Data.Set as S
 
-import AoC (Solver)
+import AoC (Solver, cartesianWith, count)
 import AoC.Grid
+import AoC.Dijkstra
 import AoC.Trace
 
 type GCoord = Coord Int
@@ -40,22 +40,25 @@ type StartCoord = IGCoord
 start :: Grid Char -> StartCoord
 start g = ((0, 0), head $ filter ((== 'S') . (g !)) $ coords g)
 
-igadd :: ICoord -> IGCoord -> IGCoord
-igadd ga (gb, b) = (add ga gb, b)
-
-type GridSize = Coord Int
-type GridGraph = M.Map GCoord [IGCoord]
-type GoEverywhere = IGCoord -> S.Set IGCoord
-
-goAll :: GridGraph -> GoEverywhere
-goAll dirmap = goAll'
-  where goAll' (gc, c) = S.fromList $ map (igadd gc) $ dirmap M.! c
-
 type Garden = (Grid Tile, StartCoord)
 
 garden :: Grid Char -> Garden
 garden g = (g', start g)
   where g' = mapG tile g
+
+data GardenType = Walled | Open
+
+gridmod :: GridSize -> GCoord -> IGCoord
+gridmod (mx, my) (x, y) = ((div x mx, div y my), (mod x mx, mod y my))
+
+iadd :: ICoord -> IGCoord -> IGCoord
+iadd ica (icb, cb) = (add ica icb, cb)
+
+gadd :: IGCoord -> GCoord -> IGCoord
+gadd (ica, ca) cb = (ica, add ca cb)
+
+igadd :: IGCoord -> IGCoord -> IGCoord
+igadd (ica, ca) (icb, cb) = (add ica icb, add ca cb)
 
 markGrid :: Grid Tile -> [GCoord] -> Grid Char
 markGrid g marks = mapG mark cg
@@ -63,104 +66,72 @@ markGrid g marks = mapG mark cg
         mark c | c `elem` marks = 'O'
                | otherwise = head $ show (g ! c)
 
-gridMarks :: Int -> Grid Tile -> [(Int, S.Set IGCoord)] -> String
-gridMarks n g = intercalate "\n\n" . map (drawGrid . markGrid g . map snd . S.toList . snd) . take n
+markGrid' :: Int -> Grid Tile -> [IGCoord] -> Grid Char
+markGrid' n g marks = Grid $ concat [map concat $ transpose [ungrid $ mapG (mark (x, y)) cg | x <- [-n..n]] | y <- [-n..n]]
+  where cg = coordsG g
+        mark ic c | (ic, c) `elem` marks = 'O'
+                  | otherwise = head $ show (g ! c)
 
--- gridMarks :: Int -> Grid Tile -> [S.Set IGCoord] -> String
--- gridMarks n g = intercalate "\n\n" . map (drawGrid . markGrid g . map snd . S.toList) . take n
+type ExtendedTileGrid = (Grid Tile, [Int], [Int])
 
--- reach :: Garden -> GoEverywhere -> Int -> Int
--- reach (g, s) go' n = fst $ memf (n + 1)
---   where mem = (0, S.empty):(1, S.singleton s):map step (zip mem (tail mem))
--- --         memf x = (ftrace (gridMarks 4 g) $ mem) !! x
---         memf x = mem !! x
---         step ((pl, ps), (_, ps')) = let cs = newcs ps ps' in (pl + S.size cs, cs)
---         newcs ps ps' = flip S.difference (S.union ps ps') $ S.unions $ S.map go' ps'
+extend :: Grid Tile -> ExtendedTileGrid
+extend g = (g, r, r)
+  where r = [-1..1]
 
-reach :: Garden -> GoEverywhere -> Int -> GoEverywhere -> Int -> Int
-reach (g, s) go' steps go'' n | n `mod` steps == 0 = fst $ memf ((div n steps) + 1)
-                              | otherwise = fst $ memf' ((div (n - 1) steps) + 1)
-  where mem = (0, S.empty):(1, S.singleton s):map step (zip mem (tail mem))
---         memf x = (ftrace (gridMarks 4 g) $ mem) !! x
-        memf x = mem !! x
-        mem' = (0, S.empty):(S.size firstcs, firstcs):map step (zip mem' (tail mem'))
---         memf' x = (ftrace (gridMarks 4 g) $ mem') !! x
-        memf' x = mem' !! x
-        step ((_, ps'), (pl, ps)) = let cs = newcs ps ps' in (pl + S.size cs, cs)
-        newcs ps ps' = flip S.difference (S.union ps ps') $ S.unions $ S.map go'' ps
-        firstcs = go' s
+extendX :: Grid Tile -> ExtendedTileGrid
+extendX g = (g, [0, 1], [0])
 
--- reach :: Garden -> GoEverywhere -> Int -> GoEverywhere -> Int -> Int
--- reach (g, s) go' steps go'' n | n `mod` steps == 0 = S.size $ memf (div n steps)
---                               | otherwise = S.size $ memf' (div (n - 1) steps)
---   where mem = (S.singleton s):map step mem
--- --         memf x = (ftrace (gridMarks 4 g) $ mem) !! x
---         memf x = mem !! x
---         mem' = firstcs:map step mem'
--- --         memf' x = (ftrace (gridMarks 4 g) $ mem') !! x
---         memf' x = mem' !! x
---         step ps = S.unions $ S.map go'' ps
---         firstcs = go' s
+extendY :: Grid Tile -> ExtendedTileGrid
+extendY g = (g, [0], [0, 1])
 
-reach' :: Garden -> Int -> GoEverywhere -> Int -> Int
-reach' (g, s) steps go' n | even steps = fst $ memf ((div n steps) + 1)
-                          | otherwise = fst $ memf' ((div n steps) + 1)
-  where mem = (0, S.empty):(1, S.singleton s):map step (zip mem (tail mem))
---         memf x = (ftrace (gridMarks 4 g) $ mem) !! x
-        memf x = mem !! x
-        step ((_, ps'), (pl, ps)) = let cs = newcs ps ps' in (pl + S.size cs, cs)
-        mem' = (0, S.empty):(1, S.singleton s):map step' (zip mem (tail mem))
---         memf' x = (ftrace (gridMarks 4 g) $ mem') !! x
-        memf' x = mem' !! x
-        step' ((pl', ps'), (_, ps)) = let cs = newcs ps ps' in (pl' + S.size cs, cs)
-        newcs ps ps' = flip S.difference (S.union ps ps') $ S.unions $ S.map go' ps
-
-dirs :: Grid Tile -> GridGraph
-dirs g = M.fromList [(c, [d | d <- map ((,) (0, 0) . flip go c) [D, R, U, L], snd d `elem` p]) | c <- p]
-  where p = plots g
-
-dirsn :: Int -> GridGraph -> GridGraph
-dirsn 1 dirmap1 = dirmap1
-dirsn n dirmap1 = M.mapWithKey gon dirmapn
-  where go' = goAll dirmap1
-        gon c = filter ((/= c) . snd) . S.toList . S.unions . map go'
-        dirmapn = dirsn (n - 1) dirmap1
+-- <!-- Dijkstra stuff
+instance GName IGCoord
+instance GGraph ExtendedTileGrid where
+  type GNodeName ExtendedTileGrid = IGCoord
+  nodes (g, xr, yr) = [((x, y), c) | c <- plots g, x <- xr, y <- yr]
+  edges (g, xr, yr) (ic, c) = let s = size g in [(d, 1) | d <- map (iadd ic . gridmod s . flip go c) [D, R, U, L], elem (fst $ fst d) xr, elem (snd $ fst d) yr, isPlot g $ snd d]
+-- -->
+ 
+reach :: GardenType -> Garden -> Int -> Int
+reach Walled (g, is) n = length reachables
+-- reach Walled (g, is) n = length $ ftrace (drawGrid . markGrid g . map snd) reachables
+  where reachables = filter dg $ map (gadd is) $ cartesianWith add diag $ map neg diag'
+        diag = zip [0..n] $ reverse [0..n]
+        diag' = zip [0..n] [0..n]
+        dg = dijkstraMax (extend g) is (Dist n)
+reach Open (g, is@(_, s)) n = length reachables
+-- reach Open (g, is@(_, s)) n = length $ ftrace (drawGrid . markGrid' 2 g) reachables
+-- tip: manhattan distance to repeat parts outside of the 3x3 square surrounding the original garden will always be == width/height of the original garden because there is a straight path to them
+-- this is not true for the test input, though, so calculate distance from one edge to the next to add outside the 3x3 square
+-- limit testable coordinates to the 5x5 square ?
+  where reachables = filter ((<= (Dist n)) . dg') $ filter (isPlot g . snd) $ map (gm . add s) $ cartesianWith add diag $ map neg diag'
+        reachables' = count ((==) (0, 0) . fst) reachables
+        (sx, sy) = size g
+        max1 i = signum i * (min 1 $ abs i)
+        min0 i = Dist $ max 0 $ abs i - 1
+        dg' ((ix, iy), c) = let (dx, dy) = ad c in dx * min0 ix + dy * min0 iy + dg ((max1 ix, max1 iy), c)
+        nx = (min 2 $ n `div` sx) * sx + n `mod` sx
+        ny = (min 2 $ n `div` sy) * sy + n `mod` sy
+        diag = zip [0..nx] $ reverse [0..ny]
+        diag' = zip [0..nx] [0..ny]
+        gm = gridmod $ size g
+        dg = dijkstra (extend g) is
+        p = plots g
+        ad = (M.!) additionalDistances
+        additionalDistances = M.fromList $ zip p $ map additionalDistance p
+        additionalDistance c = (dijkstraX ((0, 0), c) ((1, 0), c), dijkstraY ((0, 0), c) ((0, 1), c))
+        dijkstraX = dijkstra $ extendX g
+        dijkstraY = dijkstra $ extendY g
 
 part1 :: Solver
-part1 input = show $ map solve [6,64]
---   where solve = rtrace $ reach g' (goAll dirmap1)
-  where solve = rtrace $ reach g' (goAll dirmap1) 2 (goAll dirmap2)
-        dirmap1 = dirs $ fst g'
-        dirmap2 = dirsn 2 dirmap1
-        g' = garden g
-        g = Grid $ lines input
+part1 input = show $ map solve [6, 64]
+  where solve = rtrace $ reach Walled g'
+        g' = garden $ Grid $ lines input
 
-gridmod :: GridSize -> GCoord -> IGCoord
-gridmod (mx, my) (x, y) = ((div x mx, div y my), (mod x mx, mod y my))
-
-dirs' :: Grid Tile -> GridGraph
-dirs' g = M.fromList [(c, [d | d <- map (gm . flip go c) [D, R, U, L], snd d `elem` p]) | c <- p]
-  where p = plots g
-        gm = gridmod $ size g
+type GridSize = Coord Int
 
 part2 :: Solver
-part2 input = show $ map solve [26501365]
--- part2 input = show $ map solve [6, 10, 50, 100, 500, 1000, 5000, 26501365]
--- part2 input = show $ map solve [6, 10, 50, 100, 500, 1000]
--- part2 input = show $ map solve [10, 50, 100, 500, 1000]
--- part2 input = show $ map solve [3, 6]
---   where solve = rtrace $ reach g' (goAll dirmap1)
---   where solve = rtrace $ reach g' (goAll dirmap1) 2 (goAll dirmap2)
-  where solve = rtrace $ reach' g' 55 (goAll dirmap55)
---   where solve = rtrace $ reach' g' 10 (goAll dirmap10)
---   where solve = rtrace $ reach' g' 5 (goAll dirmap5)
---   where solve = rtrace $ reach' g' 3 (goAll dirmap3)
-        dirmap1 = dirs' $ fst g'
-        dirmap2 = dirsn 2 dirmap1
-        dirmap3 = dirsn 3 dirmap1
-        dirmap5 = dirsn 5 dirmap1
-        dirmap10 = dirsn 10 dirmap1
-        dirmap55 = dirsn 55 dirmap1
-        g' = garden g
-        g = Grid $ lines input
+part2 input = show $ map solve [6, 10, 50, 100, 500, 1000, 5000, 26501365]
+  where solve = rtrace $ reach Open g'
+        g' = garden $ Grid $ lines input
 
