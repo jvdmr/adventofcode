901a0a72710cae4aef9c5d685f5d6d55d483561f
Joris Vandermeersch <joris@vdmr.be>
2024-12-01 07:09:27 +0100

    24/01 ✅

--- src/AoC2024/Day01.hs
+++ src/AoC2024/Day01.hs
@@ -4,13 +4,20 @@ module AoC2024.Day01
   , part2
   ) where
 
--- import Data.List
+import Data.List (transpose, sort)
 
 import AoC (Solver)
 
+uncurryL :: (a -> a -> b) -> [a] -> b
+uncurryL f (a:b:_) = f a b
+
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . sum . map abs . uncurryL (zipWith (-)) . map sort . transpose . map (map read . words) . lines
+
+similarity :: [Int] -> [Int] -> [Int]
+similarity [] _ = []
+similarity (a:as) bs = ((a *) $ length $ filter (== a) bs):similarity as bs
 
 part2 :: Solver
-part2 = ("Not yet solved! " ++) . show . length . lines
+part2 = show . sum . uncurryL similarity . transpose . map (map read . words) . lines
 
