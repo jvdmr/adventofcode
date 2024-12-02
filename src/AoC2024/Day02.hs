a0bbaca766f9f8122ca37dd288a2f9f1a4434d68
Joris Vandermeersch <joris@vdmr.be>
2024-12-02 08:50:17 +0100

    24/02 ✅

--- src/AoC2024/Day02.hs
+++ src/AoC2024/Day02.hs
@@ -4,13 +4,30 @@ module AoC2024.Day02
   , part2
   ) where
 
--- import Data.List
-
 import AoC (Solver)
+import AoC.Util (between, count)
+
+zipTailWith :: (a -> a -> b) -> [a] -> [b]
+zipTailWith f a = zipWith f a $ tail a
+
+check :: [Int] -> Bool
+check diffs@(a:_) = btwn && sgn
+  where btwn = all (between 1 3 . abs) diffs
+        sgn = all (> 0) $ map (* a) diffs
+
+safe :: [Int] -> Bool
+safe = check . zipTailWith (-)
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . count safe . map (map read . words) . lines
+
+skipOne :: [a] -> [[a]]
+skipOne [] = [[]]
+skipOne (a:as) = as:map (a:) (skipOne as)
+
+safe' :: [Int] -> Bool
+safe' levels = any safe $ levels:skipOne levels
 
 part2 :: Solver
-part2 = ("Not yet solved! " ++) . show . length . lines
+part2 = show . count safe' . map (map read . words) . lines
 
