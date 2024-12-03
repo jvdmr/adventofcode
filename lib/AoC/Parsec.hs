c19029cd3f1b867564f2900d6dbe70e1f5cc5806
Joris Vandermeersch <joris@vdmr.be>
2024-12-03 07:16:30 +0100

    more parsec helpers

--- lib/AoC/Parsec.hs
+++ lib/AoC/Parsec.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE FlexibleInstances, TypeSynonymInstances, FlexibleContexts #-}
 module AoC.Parsec
   ( module Text.Parsec
-  , parens
   , brackets
   , braces
   , commaSep
+  , integer
+  , parens
+  , right
   ) where
 
 import Text.Parsec
+
+right :: Show a => Either a b -> b
+right (Right ast) = ast
+right (Left x) = error $ show x
 
 parens :: Stream s m Char => ParsecT s u m a -> ParsecT s u m a
 parens = between (char '(') (char ')')
@@ -20,4 +26,7 @@ commaSep = flip sepBy (char ',')
 
 braces :: Stream s m Char => ParsecT s u m a -> ParsecT s u m a
 braces = between (char '{') (char '}')
+
+integer :: Stream s m Char => ParsecT s u m Int
+integer = many1 digit >>= return . read
 
