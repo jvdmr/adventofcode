d0ab0827bc82e1b1ec286bd5e6839d12d9d0d6df
Joris Vandermeersch <joris@vdmr.be>
2024-12-04 11:14:46 +0100

    24/04 ✅

--- src/AoC2024/Day04.hs
+++ src/AoC2024/Day04.hs
@@ -5,16 +5,39 @@ module AoC2024.Day04
   , test
   ) where
 
--- import Data.List
+import Data.List (transpose)
+import Data.List.Split (splitOn)
 
 import AoC (Solver, Test)
+import AoC.Trace
+
+diag :: a -> [[a]] -> [[a]]
+diag fill = transpose . ds . transpose
+  where ds [] = []
+        ds (c:cs) = c:ds (map (fill:) cs)
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . sum . map countW . transform . lines
+  where countW = sum . map (flip (-) 1 . length . splitOn "XMAS")
+        transform g = map ($ g) [id, map reverse, transpose, transpose . reverse, df, map reverse . df, df . reverse, map reverse . df . reverse]
+        df = diag ' '
+
+type Chunk = [[Char]]
+
+chunks :: Int -> Int -> [[Char]] -> [Chunk]
+chunks x y cs | length cs < y = []
+              | length (head cs) < x = []
+              | otherwise = (map (take x) $ take y cs):(chunks x y $ map tail $ take y cs) ++ chunks x y (tail cs)
+
+xmas :: Chunk -> Bool
+xmas c = any (== "MMASS") $ map (cutx . ($ c)) [df, map reverse . df, df . map reverse, map reverse . df . reverse]
+  where cutx [a, _, b, _, c] = concat [a, b, c]
+        df = map (filter (/= ' ')) . diag ' '
 
 test :: Test
-test = show . length . lines
+test = out . filter xmas . chunks 3 3 . lines
+  where out cs = "\n" ++ (showGrids $ take 10 cs) ++ "\nTotal chunks: " ++ show (length cs)
 
 part2 :: Solver
-part2 = ("Not yet solved! " ++) . show . length . lines
+part2 = show . length . filter xmas . chunks 3 3 . lines
 
