cbdc3e284d0882d7332844324c0f99bb863e0c21
Joris Vandermeersch <joris@vdmr.be>
2024-12-05 07:35:47 +0100

    24/05 ✅

--- src/AoC2024/Day05.hs
+++ src/AoC2024/Day05.hs
@@ -5,16 +5,45 @@ module AoC2024.Day05
   , test
   ) where
 
--- import Data.List
+import Data.List.Split (splitOn)
+import Data.List (sortBy)
 
 import AoC (Solver, Test)
+import AoC.Util (pair)
+
+type Page = Int
+type Rules = (Page -> Page -> Ordering)
+type Updates = [Page]
+
+rulify :: [String] -> Rules
+rulify ls = rules
+  where rulelst = map (pair . map read . splitOn "|") ls
+        rules a b | (a, b) `elem` rulelst = LT
+                  | (b, a) `elem` rulelst = GT
+                  | otherwise = EQ
+
+updatify :: String -> [Page]
+updatify = map read . splitOn ","
+
+parseInput :: [String] -> (Rules, [Updates])
+parseInput ls = (rulify a, map (updatify) b)
+  where [a, b] = splitOn [""] ls
+
+checkUpdates :: (Rules, [Updates]) -> [Updates]
+checkUpdates (rules, updates) = map fst $ filter (uncurry (==)) $ zip updates $ map (sortBy rules) updates
+
+middle :: Updates -> Int
+middle lst = lst !! div (length lst) 2
 
 test :: Test
 test = show . length . lines
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . sum . map middle . checkUpdates . parseInput . lines
+
+sortUpdates :: (Rules, [Updates]) -> [Updates]
+sortUpdates (rules, updates) = map snd $ filter (uncurry (/=)) $ zip updates $ map (sortBy rules) updates
 
 part2 :: Solver
-part2 = ("Not yet solved! " ++) . show . length . lines
+part2 = show . sum . map middle . sortUpdates . parseInput . lines
 
