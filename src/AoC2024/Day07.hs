55b4418a40d67b975fa6e02c9a9b9cfdf68857f5
Joris Vandermeersch <joris@vdmr.be>
2024-12-07 09:48:34 +0100

    24/07 ✅

--- src/AoC2024/Day07.hs
+++ src/AoC2024/Day07.hs
@@ -5,16 +5,32 @@ module AoC2024.Day07
   , test
   ) where
 
--- import Data.List
+import Data.List.Split (splitOn)
 
 import AoC (Solver, Test)
+import AoC.Util (orF)
+
+parseInput :: String -> [Int]
+parseInput = map read . splitOn " " . filter (/= ':')
+
+type Operator = (Int -> Int -> Int)
+
+match :: [Operator] -> Int -> [Int] -> Bool
+match _ result [value] = result == value
+match ops result (a:b:vs) = orF (a, b) $ map ((.) (match ops result)) $ map ((.) (:vs)) $ map (uncurry) ops
+
+possiblyTrue :: [Operator] -> [Int] -> Bool
+possiblyTrue ops (result:values) = match ops result values
 
 test :: Test
 test = show . length . lines
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . sum . map head . filter (possiblyTrue [(+), (*)]) . map parseInput . lines
+
+(|||) :: Operator
+(|||) a b = read $ show a ++ show b
 
 part2 :: Solver
-part2 = ("Not yet solved! " ++) . show . length . lines
+part2 = show . sum . map head . filter (possiblyTrue [(+), (*), (|||)]) . map parseInput . lines
 
