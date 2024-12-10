b31f0a6daa3ed05ee9c76e43a86f358e4ab7e80b
Joris Vandermeersch <joris@vdmr.be>
2024-12-10 07:18:09 +0100

    24/10 ✅

--- src/AoC2024/Day10.hs
+++ src/AoC2024/Day10.hs
@@ -5,16 +5,44 @@ module AoC2024.Day10
   , test
   ) where
 
--- import Data.List
+import Data.List (nub)
 
 import AoC (Solver, Test)
+import AoC.Util (readChar)
+import AoC.Grid
+import AoC.Bfs (bfs)
+
+type HikeMap = Grid Int
+type Step = Coord Int
+
+isTrailHead :: HikeMap -> Step -> Bool
+isTrailHead g c = 0 == g ! c
+
+trailHeads :: HikeMap -> [Step]
+trailHeads g = filter (isTrailHead g) $ coords g
+
+nextSteps :: HikeMap -> [Step] -> [[Step]]
+nextSteps g cs = map (:cs) $ filter ((== nh) . (!) g) $ filter (inGrid g) $ map (flip go c) [U, R, D, L]
+  where nh = 1 + g ! c
+        c = head cs
+
+trails :: HikeMap -> Step -> [[Step]]
+trails g c = map fst $ filter ((== 9) . snd) $ bfs (nextSteps g) [c]
+
+rating :: HikeMap -> Step -> Int
+rating g c = length $ trails g c
+
+score :: HikeMap -> Step -> Int
+score g c = length $ nub $ map head $ trails g c
 
 test :: Test
 test = show . length . lines
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . sum . scores . mapG readChar . Grid . lines
+  where scores g = map (score g) $ trailHeads g
 
 part2 :: Solver
-part2 = ("Not yet solved! " ++) . show . length . lines
+part2 = show . sum . ratings . mapG readChar . Grid . lines
+  where ratings g = map (rating g) $ trailHeads g
 
