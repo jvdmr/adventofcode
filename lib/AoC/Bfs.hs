da071b216e112913e0ff7055b09e08e34c9e02c6
Joris Vandermeersch <joris@vdmr.be>
2024-12-12 07:11:46 +0100

    Add bfsDisconnected function to find all disconnected regions in a graph

--- lib/AoC/Bfs.hs
+++ lib/AoC/Bfs.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleInstances, TypeSynonymInstances, FlexibleContexts #-}
 module AoC.Bfs
   ( bfs
+  , bfsDisconnected
   , bfsState
   ) where
 
@@ -8,6 +9,12 @@ bfs :: (Eq a) => (a -> [a]) -> a -> [(a, Int)]
 bfs neighbors start = bfs' [start] [(start, 0)]
   where bfs' _ [] = []
         bfs' seen (node@(n, d):queue) = node:let nbs = filter (flip notElem seen) $ neighbors n in bfs' (nbs ++ seen) $ queue ++ map (flip (,) (d + 1)) nbs
+
+bfsDisconnected :: (Eq a) => (a -> [a]) -> [a] -> [[(a, Int)]]
+bfsDisconnected neighbors starts = bfs' [] starts
+  where bfs' results [] = results
+        bfs' results (start:starts) = let newresult = bfs neighbors start
+                                          in bfs' (newresult:results) $ filter (flip notElem $ map fst newresult) starts
 
 bfsState :: (Eq a) => b -> (b -> a -> (b, [a])) -> ([a] -> [a]) -> [a] -> [a] -> (b, [a])
 bfsState state _ _ result [] = (state, result)
