ad69e6f27815691ea186a46f153312f0646d2f87
Joris Vandermeersch <joris@vdmr.be>
2024-12-12 07:12:10 +0100

    24/12 part 1 ✅

--- src/AoC2024/Day12.hs
+++ src/AoC2024/Day12.hs
@@ -5,15 +5,25 @@ module AoC2024.Day12
   , tests
   ) where
 
--- import Data.List
-
 import AoC (Solver, Tests)
+import AoC.Grid
+import AoC.Bfs (bfsDisconnected)
+
+regions :: Grid Char -> [[(Coord Int, Int)]]
+regions g = bfsDisconnected neighbors $ coords g
+  where neighbors c = let cv = g ! c in filter ((== cv) . (g !)) $ filter (inGrid g) $ map (flip go c) [U, R, D, L]
+
+fencePrice :: [(Coord Int, Int)] -> Int
+fencePrice cs = length cs * length nns
+  where nns = concat $ map nonneighbors cs'
+        nonneighbors c = filter (flip notElem cs') $ map (flip go c) [U, R, D, L]
+        cs' = map fst cs
 
 tests :: Tests
-tests = [show . length . lines]
+tests = [show . Grid . lines]
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . sum . map fencePrice . regions . Grid . lines
 
 part2 :: Solver
 part2 = ("Not yet solved! " ++) . show . length . lines
