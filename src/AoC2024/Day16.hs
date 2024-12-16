f6ac647a578914b2a3170da9ef1f9094a6ed5495
Joris Vandermeersch <joris@vdmr.be>
2024-12-16 12:47:04 +0100

    24/16 part 1 ✅

--- src/AoC2024/Day16.hs
+++ src/AoC2024/Day16.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, FlexibleContexts, TypeFamilies #-}
 module AoC2024.Day16
   ( part1
   , part2
@@ -6,15 +6,48 @@ module AoC2024.Day16
   ) where
 
 import Prelude hiding (foldl)
--- import Data.List (foldl')
+import Data.List (sort)
 
 import AoC (Solver, Tests)
+import AoC.Grid
+import AoC.Dijkstra
+import AoC.Trace
+
+type Location = Coord Int
+
+isPath :: Grid Char -> Location -> Bool
+isPath g c = '#' /= g ! c
+
+type Maze = Grid Char
+
+distance :: Direction -> Direction -> Distance
+distance d nd | d == nd = Dist 0
+              | d == backwards nd = Dist 2000 -- 2 90° turns
+              | otherwise = Dist 1000
+
+type Path = (Direction, Location)
+instance GName Path
+instance GGraph Maze where
+  type GNodeName Maze = Path
+  nodes g = [(d, n) | n <- filter (isPath g) $ coords g, d <- [U, R, D, L]]
+  edges g (d, c) = let dist = distance d in filter (isPath g . snd . fst) [((nd, go nd c), 1 + dist nd) | nd <- [U, R, D, L]]
+
+findSE :: Maze -> (Path, Path)
+findSE g = ((R, f 'S'), (A, f 'E'))
+  where f c = head $ filter ((==) c . (!) g) $ coords g
+
+parseInput :: [String] -> (Maze, (Path, Path))
+parseInput ss = let g = Grid ss in (g, findSE g)
+
+dijkstra' :: (Maze, (Path, Path)) -> Distance
+dijkstra' (m, (s, e)) | fst e == A = head $ sort $ map (dijkstra m s) $ map (flip (,) $ snd e) [U, R, D, L]
+                      | otherwise = dijkstra m s e
 
 tests :: Tests
-tests = [show . length . lines]
+tests = [show . parseInput . lines]
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . dijkstra' . parseInput . lines
 
 part2 :: Solver
 part2 = ("Not yet solved! " ++) . show . length . lines
