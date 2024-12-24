c49b5f3e8991e16881cd0b8f7ea13e531114be11
Joris Vandermeersch <joris@vdmr.be>
2024-12-24 23:04:16 +0100

    24/23 ✅

--- src/AoC2024/Day23.hs
+++ src/AoC2024/Day23.hs
@@ -5,9 +5,34 @@ module AoC2024.Day23
   , tests
   ) where
 
-import Prelude hiding (foldl) -- use foldl' from Data.List if you need foldl
+import Prelude hiding (foldl)
+import Data.List (sort, intercalate, nub, sortBy)
+import Data.List.Split (splitOn)
+import Data.Ord (comparing)
+import Data.Map ((!))
+import qualified Data.Map as M
 
 import AoC (Solver, Tests)
+import AoC.Util (($>), unpair, Pair, toList, ($<))
+import AoC.Trace
+
+type Computer = String
+type Network = M.Map Computer [Computer]
+
+addEdge :: Network -> String -> Network
+addEdge nw s = M.alter (f a) b $ M.alter (f b) a nw
+  where [a, b] = splitOn "-" s
+        f n Nothing = Just [n]
+        f n (Just ns) = Just $ n:ns
+
+network :: [String] -> Network
+network = foldl' addEdge M.empty
+
+triads :: Char -> Network -> [[Computer]]
+triads c nw = foldl' f [] $ filter ((==) c . head) $ M.keys nw
+  where f seen n = let nbs = nw ! n in nub $ (++) seen $ map (sort . (n:)) $ concat $ map lsts $ filter (not . null . snd) $ map (($>) (filter $ flip elem nbs) . nnbs) nbs
+        nnbs n = (n, nw ! n)
+        lsts (n, ns) = map ((n:) . (:[])) ns
 
 tests :: Tests
 tests =
@@ -15,8 +40,17 @@ tests =
   ]
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . length . ftrace (intercalate "\n" . map show) . triads 't' . network . lines
+
+trueOrFalse :: (a -> Bool) -> [a] -> Pair [a]
+trueOrFalse pred = unpair reverse . foldl' f ([], [])
+  where f (ts, fs) v | pred v = (v:ts, fs)
+                     | otherwise = (ts, v:fs)
+
+largestGroup :: Network -> [Computer]
+largestGroup nw = sort $ last $ sortBy (comparing length) $ foldl' f [] $ M.keys nw
+  where f seen n = let nbs = nw ! n in ([n]:) $ concat $ toList $ map (n:) $< trueOrFalse (all $ flip elem nbs) seen
 
 part2 :: Solver
-part2 = ("Not yet solved! " ++) . show . length . lines
+part2 = show . intercalate "," . largestGroup . network . lines
 
