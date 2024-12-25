adc0d2471d584ff9b5773dcee0e4e94118c55500
Joris Vandermeersch <joris@vdmr.be>
2024-12-25 08:47:20 +0100

    24/24 part 1 ✅

--- src/AoC2024/Day24.hs
+++ src/AoC2024/Day24.hs
@@ -6,8 +6,40 @@ module AoC2024.Day24
   ) where
 
 import Prelude hiding (foldl) -- use foldl' from Data.List if you need foldl
+import Data.List (sort)
+import Data.List.Split (splitOn)
+import Data.Map ((!))
+import qualified Data.Map as M
 
 import AoC (Solver, Tests)
+import AoC.Util (binToInt, boolToBin)
+
+type Scope = M.Map String Bool
+
+initvar :: String -> (String, Bool)
+initvar s = (name, value == "1")
+  where [name, value] = splitOn ": " s
+
+initscope :: [String] -> Scope
+initscope = M.fromList . map initvar
+
+exec :: String -> Bool -> Bool -> Bool
+exec "AND" = (&&)
+exec "OR" = (||)
+exec "XOR" = (/=)
+
+makeGate :: Scope -> String -> Bool
+makeGate s gs = exec op (s ! a) (s ! b)
+  where [a, op, b] = splitOn " " gs
+
+scope :: [[String]] -> Scope
+scope [initvars, gates] = s
+  where is = initscope initvars
+        s = foldl' gate is gates
+        gate m g = let [gs, k] = splitOn " -> " g in M.insert k (makeGate s gs) m
+
+znum :: Scope -> Int
+znum s = binToInt $ boolToBin $ map ((!) s) $ reverse $ sort $ filter ((==) 'z' . head) $ M.keys s
 
 tests :: Tests
 tests =
@@ -15,7 +47,7 @@ tests =
   ]
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . znum . scope . splitOn [""] . lines
 
 part2 :: Solver
 part2 = ("Not yet solved! " ++) . show . length . lines
