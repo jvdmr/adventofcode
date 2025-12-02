ad0105bc2fdc4fdd5e2cf4b9baa8f214c64621b9
Joris Vandermeersch <joris@vdmr.be>
2025-12-02 10:58:25 +0100

    Add new lib functions

---
+++ lib/AoC/Text.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, FlexibleContexts #-}
+module AoC.Text
+  ( csl  -- comma-separated list
+  , range  -- expand <number><separator><number> to list of numbers
+  ) where
+
+import Data.List.Split (splitOn)
+
+import AoC.Util (pair)
+
+csl :: String -> [String]
+csl = splitOn ","
+
+range :: String -> String -> [Int]
+range separator = uncurry enumFromTo . pair . map read . splitOn separator
