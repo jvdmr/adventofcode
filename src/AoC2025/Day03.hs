4606c427c46367b2b881243e0df253b092f2cb31
Joris Vandermeersch <joris@vdmr.be>
2025-12-03 10:02:20 +0100

    25/03 ✅

--- src/AoC2025/Day03.hs
+++ src/AoC2025/Day03.hs
@@ -6,14 +6,25 @@ module AoC2025.Day03
   ) where
 
 import Prelude hiding (foldl) -- use foldl' instead of foldl
+import Data.List (sort)
 
 import AoC (Solver, Tests)
 
+batteries :: [Char] -> [Int]
+batteries = reverse . map (read . (:[]))
+
+joltage :: Int -> [Int] -> Int
+joltage 0 _ = 0
+joltage n bank = (max * 10^n') + joltage n' bank'
+  where n' = n - 1
+        max = last $ sort $ drop n' bank
+        bank' = reverse $ tail $ dropWhile (/= max) $ reverse bank
+
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . sum . map (joltage 2 . batteries) . lines
 
 part2 :: Solver
-part2 = ("Not yet solved! " ++) . show . length . lines
+part2 = show . sum . map (joltage 12 . batteries) . lines
 
 tests :: Tests
 tests =
