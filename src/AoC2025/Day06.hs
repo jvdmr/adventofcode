0ac385bc02388cbfa1853524a2351544ec0540f9
Joris Vandermeersch <joris@vdmr.be>
2025-12-06 11:53:15 +0100

    25/06 ✅

--- src/AoC2025/Day06.hs
+++ src/AoC2025/Day06.hs
@@ -6,14 +6,25 @@ module AoC2025.Day06
   ) where
 
 import Prelude hiding (foldl) -- use foldl' instead of foldl
+import Data.List (transpose)
+import Data.List.Split (splitOn)
 
 import AoC (Solver, Tests)
+import AoC.Util (multiply)
+
+calculate :: [String] -> Int
+calculate ("+":terms) = sum $ map read terms
+calculate ("*":factors) = multiply $ map read factors
+calculate problem = calculate $ reverse problem
 
 part1 :: Solver
-part1 = ("Not yet solved! " ++) . show . length . lines
+part1 = show . sum . map calculate . transpose . map words . lines
+
+cephalopodFormatting :: [String] -> [[String]]
+cephalopodFormatting problems = zipWith (:) (words $ last problems) $ map concat $ splitOn [[]] $ map words $ transpose $ init problems
 
 part2 :: Solver
-part2 = ("Not yet solved! " ++) . show . length . lines
+part2 = show . sum . map calculate . cephalopodFormatting . lines
 
 tests :: Tests
 tests =
